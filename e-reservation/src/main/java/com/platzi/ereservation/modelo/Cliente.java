050a01cb501e550a96a9ebc965f6eea86acfcd9d
Marco Antonio Toscano Freire <martosfre@MacBook-Pro-de-Marco.local>
2018-12-14 11:23:37 -0500

    Mapear Tablas de la base de Datos con JPA

--- e-reservation/src/main/java/com/platzi/ereservation/modelo/Cliente.java
+++ e-reservation/src/main/java/com/platzi/ereservation/modelo/Cliente.java
@@ -5,6 +5,14 @@ package com.platzi.ereservation.modelo;
 
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
 import lombok.Data;
 
 /**
@@ -13,7 +21,12 @@ import lombok.Data;
  *
  */
 @Data
+@Entity
+@Table(name = "cliente")
 public class Cliente {
+	@Id
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy="uuid2")
 	private String idCli;
 	private String nombreCli;
 	private String apellidoCli;
@@ -22,6 +35,7 @@ public class Cliente {
 	private String telefonoCli;
 	private String emailCli;
 	
+	@OneToMany(mappedBy="cliente")
 	private Set<Reserva> reservas;
 	
 	public Cliente() {
--- e-reservation/src/main/java/com/platzi/ereservation/modelo/Reserva.java
+++ e-reservation/src/main/java/com/platzi/ereservation/modelo/Reserva.java
@@ -5,6 +5,15 @@ package com.platzi.ereservation.modelo;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
 import lombok.Data;
 
 /**
@@ -13,11 +22,18 @@ import lombok.Data;
  *
  */
 @Data
+@Entity
+@Table(name="reserva")
 public class Reserva {
+	@Id
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy="uuid2")
 	private String idRes;
 	private Date fechaIngresoRes;
 	private Date fechaSalidaRes;
 	private int cantidadPersonasRes;
 	private String descripcionRes;
+	@ManyToOne
+	@JoinColumn(name="idCli")
 	private Cliente cliente;
 }
