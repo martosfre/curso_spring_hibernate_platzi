4f4c35430469084539861a585039c320cc6d3352
Marco Antonio Toscano Freire <martosfre@MacBook-Pro-de-Marco.local>
2018-12-14 18:53:35 -0500

    Realizar operaciones de consulta utilizando Spring Data JPA

--- e-reservation/src/main/java/com/platzi/ereservation/negocio/repository/ClienteRepository.java
+++ e-reservation/src/main/java/com/platzi/ereservation/negocio/repository/ClienteRepository.java
@@ -3,7 +3,11 @@
  */
 package com.platzi.ereservation.negocio.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.platzi.ereservation.modelo.Cliente;
 
@@ -13,5 +17,27 @@ import com.platzi.ereservation.modelo.Cliente;
  *
  */
 public interface ClienteRepository extends JpaRepository<Cliente, String>{
+
+	/**
+	 * Definición de método para buscar los clientes por su apellido
+	 * @param apellidoCli
+	 * @return
+	 */
+	public List<Cliente> findByApellidoCli(String apellidoCli);
+	
+	/**
+	 * Definición de método para buscar un cliente por su identificación
+	 * @param identificacionCli
+	 * @return
+	 */
+	public Cliente findByIdentificacion(String identificacionCli);
+	
+	/**
+	 * Definición de método para buscar un cliente por su cuenta de email
+	 * @param email
+	 * @return
+	 */
+	@Query("Select c from Cliente c where c.emailCli like %:email")
+	public Cliente findByEmailAccount(@Param("email") String email);
 	
 }
--- e-reservation/src/main/java/com/platzi/ereservation/negocio/repository/ReservaRepository.java
+++ e-reservation/src/main/java/com/platzi/ereservation/negocio/repository/ReservaRepository.java
@@ -3,8 +3,14 @@
  */
 package com.platzi.ereservation.negocio.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.platzi.ereservation.modelo.Cliente;
 import com.platzi.ereservation.modelo.Reserva;
 
 /**
@@ -12,4 +18,24 @@ import com.platzi.ereservation.modelo.Reserva;
  *
  */
 public interface ReservaRepository extends JpaRepository<Reserva, String> {
+
+	/**
+	 * Método para consultar las reservas por cliente
+	 * 
+	 * @param cliente
+	 * @return
+	 */
+	@Query("Select r from Reserva r where r.cliente =:cliente")
+	public List<Reserva> findByCliente(Cliente cliente);
+	
+	/**
+	 * Definición de método para consultar las reservas por la fecha de ingreso en
+	 * base a un rango de fechas
+	 * 
+	 * @param fechaInicio
+	 * @param fechaSalida
+	 * @return
+	 */
+	@Query("Select r from Reserva r where r.fechaIngresoRes =:fechaInicio  and r.fechaSalidaRes =:fechaSalida")
+	public List<Reserva> find(@Param("fechaInicio") Date fechaInicio, @Param("fechaSalida") Date fechaSalida);
 }
