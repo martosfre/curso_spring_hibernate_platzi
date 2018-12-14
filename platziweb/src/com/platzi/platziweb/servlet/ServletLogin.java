ad55f4ea7261ff959ad77f9e24455bb4166faeeb
Marco Antonio Toscano Freire <martosfre@MacBook-Pro-de-Marco.local>
2018-12-14 10:10:07 -0500

    Como procesar peticiones y generar contenido dinámico con Servlets

---
+++ platziweb/src/com/platzi/platziweb/servlet/ServletLogin.java
@@ -0,0 +1,54 @@
+package com.platzi.platziweb.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletLogin
+ */
+@WebServlet(description = "Servlet para manipular la petición de login", urlPatterns = { "/ServletLogin" })
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ServletLogin() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//Procesar la información
+		String usuario = request.getParameter("usuario");
+		String empresa = request.getParameter("empresa");
+		PrintWriter escritor = response.getWriter();
+		if (usuario != null && empresa != null) {
+			if(empresa.equals("Platzi")) {
+				escritor.println("Bienvenido a Platzi");
+			}else {
+				escritor.println("Bienvenido...");
+			}
+		}else {
+			escritor.println("Usuario Incorrecto!!");
+		}
+		escritor.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
