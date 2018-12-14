96f2f6a86e7377cfa831656c2d81681f2bbfc26c
Marco Antonio Toscano Freire <martosfre@MacBook-Pro-de-Marco.local>
2018-12-14 10:25:57 -0500

    Creación Proyecto Spring Boot e-reservation

---
+++ e-reservation/src/main/java/com/platzi/ereservation/EReservationApplication.java
@@ -0,0 +1,14 @@
+package com.platzi.ereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EReservationApplication.class, args);
+	}
+
+}
+
---
+++ e-reservation/src/test/java/com/platzi/ereservation/EReservationApplicationTests.java
@@ -0,0 +1,17 @@
+package com.platzi.ereservation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EReservationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
