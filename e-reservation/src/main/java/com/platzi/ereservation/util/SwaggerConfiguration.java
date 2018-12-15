213f3f60b64c974b47afc59782143938d42558ae
Marco Antonio Toscano Freire <martosfre@MacBook-Pro-de-Marco.local>
2018-12-15 12:00:37 -0500

    Instalar y Configurar Swagger

---
+++ e-reservation/src/main/java/com/platzi/ereservation/util/SwaggerConfiguration.java
@@ -0,0 +1,26 @@
+package com.platzi.ereservation.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Clase para configurar Swagger
+ * 
+ * @author martosfre
+ *
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+	public Docket documentation() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+				.build();
+	}
+}
