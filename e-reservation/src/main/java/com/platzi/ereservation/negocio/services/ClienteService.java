d41ceea4afed64d6d9b9adbc1029fbb456b71762
Marco Antonio Toscano Freire <martosfre@MacBook-Pro-de-Marco.local>
2018-12-15 10:01:10 -0500

    Implementación operaciones servicio de negocio CRUD

---
+++ e-reservation/src/main/java/com/platzi/ereservation/negocio/services/ClienteService.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package com.platzi.ereservation.negocio.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.platzi.ereservation.modelo.Cliente;
+import com.platzi.ereservation.negocio.repository.ClienteRepository;
+
+/**
+ * Clase para definir los servicios de cliente
+ * 
+ * @author martosfre
+ *
+ */
+@Service
+public class ClienteService {
+	private final ClienteRepository clienteRepository;
+
+	public ClienteService(ClienteRepository clienteRepository) {
+		this.clienteRepository = clienteRepository;
+	}
+
+	/**
+	 * Método para realizar la operación de guardar un cliente
+	 * 
+	 * @param cliente
+	 * @return
+	 */
+	public Cliente create(Cliente cliente) {
+		return this.clienteRepository.save(cliente);
+	}
+
+	/**
+	 * Método para realizar la operación de actualizar un cliente
+	 * 
+	 * @param cliente
+	 * @return
+	 */
+	public Cliente update(Cliente cliente) {
+		return this.clienteRepository.save(cliente);
+	}
+
+	/**
+	 * Método para realizar la operación de eliminar un cliente
+	 * 
+	 * @param cliente
+	 */
+	public void delete(Cliente cliente) {
+		this.clienteRepository.delete(cliente);
+	}
+
+	/**
+	 * Método para consultar un cliente por su identificación
+	 * 
+	 * @param identificacionCli
+	 * @return
+	 */
+	public Cliente findByIdentificacion(String identificacionCli) {
+		return this.clienteRepository.findByIdentificacion(identificacionCli);
+	}
+	
+	/**
+	 * Método para consultar todos los clientes
+	 * @return
+	 */
+	public List<Cliente> findAll(){
+		return this.clienteRepository.findAll();
+	}
+	
+	/**
+	 * Método para buscar los clientes por su apellido
+	 * @param apellidoCli
+	 * @return
+	 */
+	public List<Cliente> findByApellidoCli(String apellidoCli){
+		return this.clienteRepository.findByApellidoCli(apellidoCli);
+	}
+	
+	
+	/**
+	 * Método para buscar un cliente por su cuenta de email
+	 * @param email
+	 * @return
+	 */
+	public Cliente findByEmailAccount(String email) {
+		return this.findByEmailAccount(email);
+	}
+
+}
---
+++ e-reservation/src/main/java/com/platzi/ereservation/negocio/services/ReservaService.java
@@ -0,0 +1,86 @@
+package com.platzi.ereservation.negocio.services;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.platzi.ereservation.modelo.Cliente;
+import com.platzi.ereservation.modelo.Reserva;
+import com.platzi.ereservation.negocio.repository.ReservaRepository;
+
+/**
+ * Clase para definir los servicios de reserva
+ * @author martosfre
+ *
+ */
+@Service
+public class ReservaService {
+
+	private final ReservaRepository reservaRepository;
+	
+	public ReservaService(ReservaRepository reservaRepository) {
+		this.reservaRepository = reservaRepository;
+	}
+	
+	/**
+	 * Método para realizar la operación de guardar una reserva
+	 * 
+	 * @param reserva
+	 * @return
+	 */
+	public Reserva create(Reserva reserva) {
+		return this.reservaRepository.save(reserva);
+	}
+
+	/**
+	 * Método para realizar la operación de actualizar una reserva
+	 * 
+	 * @param reserva
+	 * @return
+	 */
+	public Reserva update(Reserva reserva) {
+		return this.reservaRepository.save(reserva);
+	}
+
+	/**
+	 * Método para realizar la operación de eliminar una reserva
+	 * 
+	 * @param reserva
+	 */
+	public void delete(Reserva reserva) {
+		this.reservaRepository.delete(reserva);
+	}
+
+	
+	/**
+	 * Método para consultar todos las reservas
+	 * @return
+	 */
+	public List<Reserva> findAll(){
+		return this.reservaRepository.findAll();
+	}
+	
+	/**
+	 * Método para consultar las reservas por un cliente en específico
+	 * 
+	 * @param cliente
+	 * @return
+	 */
+	public List<Reserva> findByCliente(Cliente cliente){
+		return this.reservaRepository.findByCliente(cliente);
+	}
+	
+	/**
+	 * Método para consultar las reservas por la fecha de ingreso en
+	 * base a un rango de fechas
+	 * 
+	 * @param fechaInicio
+	 * @param fechaSalida
+	 * @return
+	 */
+	public List<Reserva> find(Date fechaInicio, Date fechaSalida){
+		return this.reservaRepository.find(fechaInicio, fechaSalida);
+	}
+	
+}
